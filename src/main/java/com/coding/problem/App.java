28bac56433b93c2791247dcc850bf44580476978
SKGitHub2017 <trsk2016@gmail.com>
2016-12-23 16:17:32 +0700

    Revise code structure

--- src/main/java/com/coding/problem/App.java
+++ src/main/java/com/coding/problem/App.java
@@ -4,9 +4,8 @@ package com.coding.problem;
  * Initial Project
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello this is home test..." );
     }
 }
